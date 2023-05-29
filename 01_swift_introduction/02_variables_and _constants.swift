56fb63eef5b82193ffcd1ccca4994b614c88356f
bhavikjadav <bhavik.jadav89@gmail.com>
2023-05-29 13:23:09 +0530

    version 1

---
+++ 01_swift_introduction/02_variables_and _constants.swift
@@ -0,0 +1,32 @@
+// Learned From https://www.programiz.com/swift-programming/variables-constants-literals
+
+// Integers in Swift
+var num = 10
+print(num)
+
+// Strings in Swift
+var name:String // Type declaration
+name = "Bhavik Jadav" // Value Assigning
+print(name)
+
+var name2 = "Mehul" // Direct Assigning value without declaring the type of the variable
+print(name2)
+
+name = "B C Jadav" // Changing the value of the variable (var is Mutable)
+print(name)
+
+// Swift Constants
+let final_name = "C Jadav"
+// final_name = "BCJ"
+print(final_name)
+
+// Floating point numbers
+let marks = 98.02
+print(marks)
+
+// Booleans
+let levelPassed = false
+print(levelPassed)
+
+
+
