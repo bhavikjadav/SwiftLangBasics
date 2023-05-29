734a0d0712c05bdf40e6d0b03b23cce7471b815f
Bhavik Jadav <bhavik.jadav89@gmail.com>
2023-05-29 15:02:55 +0530

    Add files via upload

---
+++ 01_swift_introduction/01_hello_world.swift
@@ -0,0 +1,5 @@
+// This is the Swift's Single line comment.
+/*
+This is multi line comment.
+*/
+print("Hello World!")
---
+++ 01_swift_introduction/05_expressions_and_statements.swift
@@ -0,0 +1,21 @@
+/*
+Expressions = (Assigning), == (Comparison)
+*/
+
+// Simple Statement
+var score = 10*15
+print(score)
+
+// Conditional Statement
+var age = 08
+if (age < 18) {
+    print("You can't see this Movie.")
+} else {
+    print("You can see this Movie.")
+    
+}
+
+// For Loop
+for i in 1...10 {
+    print("Hello! \(i)")
+}
